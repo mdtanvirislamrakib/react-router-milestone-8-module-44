5f11259bb8c97f72290590fec19bffb408e4d0a5
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-04-13 12:04:54 +0600

    start react router module 44

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Navbar />
+        <DaisyNav />
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Link from './Link';
+
+const navigationData = [
+    {
+        "id": 1,
+        "name": "Home",
+        "path": "/"
+    },
+    {
+        "id": 2,
+        "name": "About",
+        "path": "/about"
+    },
+    {
+        "id": 3,
+        "name": "Services",
+        "path": "/services"
+    },
+    {
+        "id": 4,
+        "name": "Portfolio",
+        "path": "/portfolio"
+    },
+    {
+        "id": 5,
+        "name": "Contact",
+        "path": "/contact"
+    }
+]
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul className='flex'>
+            {
+                navigationData.map(route => <Link key={route.id} route = {route}></Link>)
+            }
+            </ul>
+            
+        </nav>
+    );
+};
+
+export default Navbar;
