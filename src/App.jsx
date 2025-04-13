31dc24e3c8ffd18720ee70e335c1dec085c12cb2
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-04-13 15:12:18 +0600

    make a responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <header>
         <Navbar />
-        <DaisyNav />
+        {/* <DaisyNav /> */}
       </header>
     </>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
+
 
 const navigationData = [
     {
@@ -31,14 +33,26 @@ const navigationData = [
 
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
+    const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
     return (
-        <nav>
-            <ul className='flex'>
-            {
-                navigationData.map(route => <Link key={route.id} route = {route}></Link>)
-            }
+        <nav className='flex justify-between items-center mx-6 mt-5'>
+            <span className='flex' onClick={() => setOpen(!open)}>
+                {open ? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>}
+                <ul className={`md:hidden absolute bg-zinc-800 ${open ? 'top-10' : '-top-40'}  duration-1000`}>
+                    {links}
+                </ul>          
+                <h3>My navbar</h3>
+            </span>
+
+            <ul className='md:flex hidden'>
+                {
+                    links  
+
+                }
             </ul>
-            
+            <button>Sign In</button>
         </nav>
     );
 };
